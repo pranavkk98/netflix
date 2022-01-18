c1f42d53afadf1ecd47d21fc193369213414b3d1
Mansoor <ali.syedmansoor1997@gmail.com>
2022-01-18 20:22:47 +0530

    Made changes in login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -25,13 +25,17 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json("Wrong password or username!");
+
+    if (!user) {
+      return res.status(401).json("Wrong password or username!");
+    }
 
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    originalPassword !== req.body.password &&
-      res.status(401).json("Wrong password or username!");
+    if (originalPassword !== req.body.password) {
+      return res.status(401).json("Wrong password or username!");
+    }
 
     const accessToken = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
